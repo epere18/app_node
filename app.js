2382ac074feba58831fae0c42217eec3a4a6d866
epere18 <epere18@hotmail.com>
2023-08-23 09:49:20 -0300

    Subiendo archivos /productos en json

--- app.js
+++ app.js
@@ -2,7 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res)=> res.send("Practicando Node.js"));
+
+app.get("/", (req, res)=>res.send("Practicando Node.js"));
+
+app.get("/productos", (req, res)=> {
+    const products = [
+        {
+            id: 1,
+            name: "Oscar",
+        },
+        {
+            id: 2,
+            name: "Andres",
+        },
+        {
+            id: 3,
+            name: "Ingrid",
+        },
+        {
+            id: 4,
+            name: "Arianna",
+        },
+        {
+            id: 5,
+            name: "Adriano",
+        },
+    ];
+    res.json(products);
+});
+
 app.listen(port,()=> console.log("Estoy corriendo en el puerto ${port}!"));
 
 
